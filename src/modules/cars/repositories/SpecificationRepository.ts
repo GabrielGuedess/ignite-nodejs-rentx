1e855a33a0a3f2c49ec4026486b4f69db4a735b2
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-09-23 01:33:52 -0300

    FEAT: adicionando rota de especificações

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,33 @@
+import { Specification } from '../model/Specification';
+import {
+  ISpecificationRepository,
+  ICreateSpecificationDTO,
+} from './ISpecificationRepository';
+
+export class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      create_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      specification => specification.name === name,
+    );
+
+    return specification;
+  }
+}
